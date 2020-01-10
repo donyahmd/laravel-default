2fc5af4d7733cbdf03aff272b95c9b39db2fd8ac
Doni Ahmad <donyahmd24@gmail.com>
2020-01-10 15:30:20 +0800

    Crud Generator

--- app/Console/Commands/CrudGenerator.php
+++ app/Console/Commands/CrudGenerator.php
@@ -11,14 +11,14 @@ class CrudGenerator extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'make:crud {name}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Membuat CRUD Otomatis';
 
     /**
      * Create a new command instance.
@@ -37,6 +37,61 @@ class CrudGenerator extends Command
      */
     public function handle()
     {
-        //
+        $name = $this->argument('name');
+
+        $this->controller($name);
+        $this->model($name);
+        $this->request($name);
+
+        File::append(base_path('routes/web.php'), 'Route::resource(\'' . str_plural(strtolower($name)) . "', '{$name}Controller');");
+    }
+
+    protected function getStub($type)
+    {
+        return file_get_contents(resource_path("stubs/$type.stub"));
+    }
+
+    protected function model($name)
+    {
+        $modelTemplate = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Model')
+        );
+
+        file_put_contents(app_path("/Models/{$name}.php"), $modelTemplate);
+    }
+
+    protected function controller($name)
+    {
+        $controllerTemplate = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}'
+            ],
+            [
+                $name,
+                strtolower(str_plural($name)),
+                strtolower($name)
+            ],
+            $this->getStub('Controller')
+        );
+
+        file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $controllerTemplate);
+    }
+
+    protected function request($name)
+    {
+        $requestTemplate = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Request')
+        );
+
+        if(!file_exists($path = app_path('/Http/Requests')))
+            mkdir($path, 0777, true);
+
+        file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $requestTemplate);
     }
 }
