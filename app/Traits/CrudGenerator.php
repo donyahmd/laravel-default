0edd3195eb2285eaaf3036cfdf065b77b7373c99
Doni Ahmad <donyahmd24@gmail.com>
2020-01-13 15:31:50 +0800

    migration with field parameter

--- app/Traits/CrudGenerator.php
+++ app/Traits/CrudGenerator.php
@@ -6,13 +6,14 @@ use Carbon\Carbon;
 use Illuminate\Support\Str;
 trait CrudGenerator
 {
-    protected function createCrud($className, $parameter = null)
+    protected function createCrud($className, $field = null)
     {
         $this->makeController($className);
         $this->makeModel($className);
         $this->makeRequest($className);
         $this->makeRoute($className);
-        $this->makeMigration($className);
+        $this->makeMigration($className, $field);
+        $this->makeView($className, $field);
     }
 
     private function getStub($type)
@@ -89,19 +90,24 @@ trait CrudGenerator
         file_put_contents($routes, $newRoutes);
     }
 
-    private function makeMigration($className)
+    private function makeMigration($className, $explodeField = null)
     {
         $migrationTableName = Str::plural(Str::lower(Str::snake($className)));
+
+        $field = $this->migrationColumnList($explodeField);
+
         $migrationTemplate = str_replace(
             [
                 '{modelName}',
                 '{modelNamePluralLowerCase}',
-                '{modelNamePlural}'
+                '{modelNamePlural}',
+                '{field}'
             ],
             [
                 $className,
                 $migrationTableName,
                 Str::plural($className),
+                $field,
             ],
             $this->getStub('Migration')
         );
@@ -113,7 +119,57 @@ trait CrudGenerator
         file_put_contents(base_path("/database/migrations/{$timestamp}_create_{$migrationTableName}_table.php"), $migrationTemplate);
     }
 
-    private function makeIndexView($className)
+    private function migrationColumnList($explodeField)
+    {
+        $migrationColumn = null;
+        foreach ($explodeField as $value) {
+            $field  = $value[0];
+            $type   = $this->typeDataColumn($value[1]);
+
+            if (isset($value[2])) {
+                $length = ', '.$value[2];
+            } else {
+                $length = '';
+            }
+
+            $migrationColumn .= "\t\t\t".'$table->'.$type."('". $field ."'".$length. ");" . PHP_EOL;
+        }
+
+        return $migrationColumn;
+    }
+
+    private function typeDataColumn($string) {
+        switch ($string) {
+            case 'int':
+                return 'integer';
+                break;
+            case 'uint':
+                return 'unsignedInteger';
+                break;
+            case 'str':
+                return 'string';
+                break;
+            case 'txt':
+                return 'txt';
+                break;
+            case 'char':
+                return 'char';
+                break;
+            case 'date':
+                return 'date';
+                break;
+            default:
+                return 'string';
+                break;
+        }
+    }
+
+    private function makeView($className, $explodeField = null)
+    {
+        $this->makeIndexView($className, $explodeField);
+    }
+
+    private function makeIndexView($className, $explodeField = null)
     {
         //TODO: Bikin auto
     }
