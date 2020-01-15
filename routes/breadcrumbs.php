6cd1fba52cf518f0130bb8e76c69f6d7ccef5105
Doni Ahmad <donyahmd24@gmail.com>
2020-01-15 23:35:31 +0800

    add example usage of breadcrumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -4,3 +4,15 @@
 Breadcrumbs::for('home', function ($trail) {
     $trail->push(config('app.name'), URL::to('/'));
 });
+
+// //Home > Berita
+// Breadcrumbs::for('berita.index', function ($trail) {
+//     $trail->parent('home');
+//     $trail->push('Berita', action('BeritaController@index'));
+// });
+
+// //Home > Berita > Create
+// Breadcrumbs::for('berita.create', function ($trail) {
+//     $trail->parent('berita.index');
+//     $trail->push('Berita.create', action('BeritaController@create'));
+// });
